e2a5a4487edac0fe9f564033f59637f19be49b7e
bfredl <bjorn.linse@gmail.com>
2023-03-14 00:18:33 +0100

    no End-of-text

--- utils.cpp
+++ utils.cpp
@@ -361,7 +361,10 @@ gpt_vocab::id llama_sample_top_p_top_k(
         for (int i = 0; i < n_logits; ++i) {
             // repetition penalty from CTRL paper (https://arxiv.org/abs/1909.05858)
             // credit https://github.com/facebookresearch/llama/compare/main...shawwn:llama:main
-            if (std::find(last_n_tokens.begin(), last_n_tokens.end(), i) != last_n_tokens.end()) {
+            if (i == 2) {
+                // no end-of-text in output. booring!
+                logits_id.push_back(std::make_pair(-500, i));
+            } else if (std::find(last_n_tokens.begin(), last_n_tokens.end(), i) != last_n_tokens.end()) {
                 // if score < 0 then repetition penalty has to multiplied to reduce the previous token probability
                 if (logits[i] < 0.0) {
                     logits_id.push_back(std::make_pair(logits[i]*scale*repeat_penalty, i));
@@ -373,6 +376,7 @@ gpt_vocab::id llama_sample_top_p_top_k(
             }
         }
     }
+
 
     sample_top_k(logits_id, top_k);
 
